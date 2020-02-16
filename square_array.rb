33dbfb4bb98b47814fe6b1cc82cb68cd6c4b239e
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-16 22:21:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,3 @@
-#def square_array(array)
-#  array.each {|numbers| numbers**2}
-#end
-
 def square_array(array)
-  squared = []
-array.each { |num| squared << num**2}
-squared
-  # your code here
+  array.each {|numbers| squared << numbers**2}
 end
